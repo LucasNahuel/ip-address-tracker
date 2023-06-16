c39aab2151d7e640fa2bef1e5f21c49bfb0c372b
LucasNahuel <munozlucas96@gmail.com>
2023-06-16 01:08:08 -0300

    added first load ip-address searching

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,8 +1,31 @@
 import iconArrow from './../assets/images/icon-arrow.svg';
-
+import  {useEffect, useState}  from 'react';
 function Banner(props){
 
     const arrow = iconArrow;
+
+    const [ipAddress, setIpAddress] = useState(null);
+
+    useEffect(()=>{
+        getAddress()
+    });
+
+
+    async function getAddress(){
+        const results = await fetch( "https://geo.ipify.org/api/v2/country,city,vpn?apiKey="+process.env.REACT_APP_IPIFY_API_KEY
+        ).then((response) => {
+            console.log(response);
+
+            if(response.ok === true){
+                return response.json();
+            }else{
+                console.log(response);
+            }
+        }).then((data) => {
+            console.log(data);
+            setIpAddress(data);
+        })
+    }
 
     function handleSubmit(){
 
@@ -13,9 +36,9 @@ function Banner(props){
         <div className="banner-root">
             <h1 className="banner-title">IP Address Tracker</h1>
 
-            <form onSubmit={()=> handleSubmit()}>
+            <form onSubmit={() => handleSubmit()}>
 
-                <input type="text" placeholder="serch for any IP address or domain"></input>
+                <input type="text" placeholder="serch for any IP address or domain"/>
 
                 <button type="submit">
                     <img src={arrow}></img>
@@ -24,7 +47,31 @@ function Banner(props){
             </form>
 
             <div className="ip-info-root">
+                <div className="ip-info-item">
+                    <p className="ip-info-item-title">IP ADRESS</p>
+                    {ipAddress ? ipAddress.ip : ""}
+                </div>
 
+                <div className='ip-info-divider'/>
+
+                <div className="ip-info-item">
+                    <p className="ip-info-item-title">LOCATION</p>
+                    {ipAddress ? ipAddress.location.city+", "+ipAddress.location.region : ""}
+                </div>
+
+                <div className='ip-info-divider'/>
+
+                <div className="ip-info-item">
+                    <p className="ip-info-item-title">TIMEZONE</p>
+                    {ipAddress ? "UTC"+ipAddress.location.timezone : ""}
+                </div>
+
+                <div className='ip-info-divider'/>
+
+                <div className="ip-info-item">
+                    <p className="ip-info-item-title">ISP</p>
+                    <div>{ ipAddress ? ipAddress.isp : ""}</div>
+                </div>
             </div>
 
 
